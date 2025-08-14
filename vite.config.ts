63704e2cf18697e91153befcb012e5bcf626dddf
TatianaMendez <tatianamendez45@gmail.com>
2025-08-14 16:12:23 -0500

    delete feredation!

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import React from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import path from 'path';
-import federation from '@originjs/vite-plugin-federation';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
@@ -10,23 +9,6 @@ export default defineConfig({
     React(),
     tsconfigPaths(),
     tailwindcss(),
-    federation({
-      name: 'appmathildeweb', // Nombre del remoto
-      filename: 'remoteEntry.js',
-      exposes: {
-        './LoginForm': './app/components/pages/login/login.tsx',
-        './RegisterForm': './app/components/pages/register/register.tsx',
-        './ResetPass': './app/components/pages/login/resetPassword/resetPassword.tsx',
-        './ValidationPass': './app/components/pages/login/resetPassword/validation.tsx',
-        './Dashboard': './app/components/pages/dashboard/dashboard.tsx',
-        './ThirdPartyCampaign': './app/components/pages/thirdPartyCampaign/thirdPartyCampaign.tsx',
-        './SettingsUser': './app/components/pages/settings/settings.tsx',
-        './autenticationService': './app/services/autenticationService.tsx',
-        './Report': './app/components/pages/report/report.tsx',
-      },
-      remotes: {},
-      shared: ['react', 'react-dom', 'react-router-dom', 'react-icons'],
-    }),
     
   ],
   resolve: {
